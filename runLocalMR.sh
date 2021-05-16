696d153ec4be45c69060d9dcb477316ecb86f804
dhuy237 <d.huy723@gmail.com>
2021-05-16 10:27:17 +0700

    modified hdfs path in run.sh

--- runLocalMR.sh
+++ runLocalMR.sh
@@ -1,12 +1,12 @@
 conda activate pyenv
 
-export DATA_PATH=bimeta/data/test
-export INPUT_FILE=R4_medium.fna
-export LENGTHS_OF_K_MERS=4
-export LENGTH_OF_Q_MERS=30
-export NUM_SHARED_READS=45
-export NUM_OF_SPECIES=2
-
+DATA_PATH=bimeta/data/test
+INPUT_FILE=R4_medium.fna
+LENGTHS_OF_K_MERS=4
+LENGTH_OF_Q_MERS=30
+NUM_SHARED_READS=45
+NUM_OF_SPECIES=2
+USR_HDFS=hdfs:///user/graphframes_cps/2
 
 # Step 1.1
 python bimeta/load_meta_reads/load_read.py $DATA_PATH/$INPUT_FILE --output $DATA_PATH/output_1_1 --time $DATA_PATH
@@ -48,14 +48,14 @@ spark-submit --packages graphframes:graphframes:0.8.1-spark3.0-s_2.12 \
 bimeta/build_overlap_graph/connected.py \
 --vertices $DATA_PATH/output_1_1/part-00000 \
 --edges $DATA_PATH/output_2_1.txt \
---checkpoint "/home/dhuy237/graphframes_cps/5" \
---output "/home/dhuy237/graphframes_cps/5/5" \
+--checkpoint $USR_HDFS \
+--output $USR_HDFS/output \
 --output_graph $DATA_PATH \
 --num_reads $NUM_SHARED_READS \
 --time $DATA_PATH
 
 mkdir $DATA_PATH/output_2_2/
-hdfs dfs -get /home/dhuy237/graphframes_cps/5/5/part-00000 $DATA_PATH/output_2_2/
+hdfs dfs -get $USR_HDFS/output/part-00000 $DATA_PATH/output_2_2/
 
 
 # Step 3
